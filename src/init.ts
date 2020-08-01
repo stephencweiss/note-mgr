84e8a70421f3c896d9fab396b53f9c40db50e2e8
Stephen <stephen.c.weiss@gmail.com>
2020-08-01 13:08:20 -0500

    🏗️ lift `.contents` to root
    instead of nesting the `.contents` file in the notes directory, it
    is now at the root which makes it more reasonable as an index file.
    
    it's position at the root will be easier to access, not to mention
    it makes more sense.

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,8 @@
 import path from "path"
-import fs from "fs"
+const fs = require("fs")
 import { prompt } from "inquirer"
 import { Command } from "commander"
-import { Config, HOME } from "./utils"
+import { Config, HOME, NOTES_ROOT_DIR } from "./utils"
 
 const fsPromises = fs.promises
 
@@ -36,21 +36,21 @@ class NoteManagerConfigurer extends Config {
      * The desired file structure is as follows:
      * .
      * └── targetDir
+     *     └── .contents.md
      *     └── notes
-     *         └── .notes.md
      * A final step is to save the results to the note-mgr config file.
      */
     configure(): void {
         const notesDir = `${this.targetPath}/notes`
         fsPromises
             .access(this.targetPath)
-            .then(() => {
-                this.notesExists(notesDir)
-            })
             .catch(() => {
                 this.createNotesDir(notesDir)
             })
-            .finally(() => this.configureSettings())
+            .finally(() => {
+                this.initializeNotesCatalogue()
+                this.configureSettings()
+            })
     }
 
     /**
@@ -78,33 +78,17 @@ class NoteManagerConfigurer extends Config {
         })
     }
 
-    notesExists(noteDir: string) {
-        const notePath = path.resolve(HOME, noteDir)
-        fsPromises
-            .access(notePath)
-            .then(() => {
-                fsPromises
-                    .access(`${notePath}/.notes.md`)
-                    .then(() => `${notePath}/.notes.md exists`)
-                    .catch(() => this.initializeNotesCatalogue(notePath))
-            })
-            .catch(() => {
-                this.createNotesDir(noteDir)
-            })
-    }
-
     private createNotesDir(notesDir: string) {
         const notesPath = path.resolve(HOME, notesDir)
         fs.mkdirSync(notesPath, { recursive: true })
-        this.initializeNotesCatalogue(notesPath)
     }
 
     /**
      * The `.notes.md` file is a catalogue of all notes
      */
-    private initializeNotesCatalogue(path: string) {
+    private initializeNotesCatalogue() {
         fs.writeFile(
-            `${path}/.notes.md`,
+            `${this.targetPath}/${ConfigurationKeys.NOTES_INDEX_FILE}.md`,
             "# Drafts\n\n# Notes\n",
             (error: Error) => {
                 if (error)
