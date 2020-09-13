1d92fcf3cf4e12a15c2bac33c84ee59137c0aa05
Stephen <stephen.c.weiss@gmail.com>
2020-09-13 12:56:59 -0500

    🐛 convert times to utc
    all times are interpretted / viewed as utc. this includes new
    file generation and updating.

--- src/utils/dateHelpers.ts
+++ src/utils/dateHelpers.ts
@@ -1,9 +1,11 @@
 import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+dayjs.extend(utc)
 
 export function isValidDt(date: string) {
     return dayjs(date).isValid()
 }
 
 export function formatDt(date: string, format?: string) {
-    return dayjs(date).format(format)
+    return dayjs(date).utc().format(format)
 }
