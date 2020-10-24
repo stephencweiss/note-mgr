82179d546084792ba439b5a5ec01a3ca19763cd9
Stephen <stephen.c.weiss@gmail.com>
2020-10-24 11:58:49 -0500

    🟢 basic init test
    init passes a basic test of being executed and exiting successfully

--- packages/nom/test/jest/init.ts
+++ packages/nom/test/jest/init.ts
@@ -1,42 +1,31 @@
 import { test } from "@oclif/test"
-import { Volume } from "memfs"
 
-// jest will mock any import of the fs module
-jest.mock("fs", () => {
-    const fs = jest.requireActual("fs")
-    const unionfs = require("unionfs").default
-    return unionfs.use(fs)
-})
-
-beforeEach(() => {
-    jest.resetAllMocks()
-})
 // given when then
-describe("Init command", () => {
+describe("Init command can receive an arg for a target directory", () => {
     test.stdout()
-        .command(["init"])
-        .it("creates a notes directory if one does not exist", (ctx) => {})
-    test.stdout()
-        .command(["init"])
-        .it(
-            "creates a `.contents` file within the notes directory",
-            (ctx) => {}
-        )
-    test.stdout()
-        .command(["init"])
-        .it("creates a default config file in the home directory", (ctx) => {})
-})
-
-describe("init command works ok", () => {
-    test.stdout()
-        .command(["init"]) // the command
-        .it("runs init", (ctx) => {
-            expect(ctx.stdout).toBe("hello world from ./src/commands/init.ts\n")
-        })
-
-    test.stdout()
-        .command(["init", "--name", "jeff"])
-        .it("runs init --name jeff", (ctx) => {
-            expect(ctx.stdout).toBe("hello jeff from ./src/commands/init.ts\n")
+        .command(["init", "path"])
+        .it("exits successfully", async (ctx) => {
+            expect(ctx.stdout).toBe(
+                "Successfully initialized nom! Welcome to a more pleasant life!\n"
+            )
         })
 })
+
+// describe("Init command", () => {
+//     test.stdout()
+//         .command(["init"])
+//         .it("creates a notes directory if one does not exist", async (ctx) => {
+//             await ctx
+//             console.log(JSON.stringify(ctx, null, 4))
+//             expect(ctx).toBeFalsy()
+//         })
+//     test.stdout()
+//         .command(["init"])
+//         .it(
+//             "creates a `.contents` file within the notes directory",
+//             (ctx) => {}
+//         )
+//     test.stdout()
+//         .command(["init"])
+//         .it("creates a default config file in the home directory", (ctx) => {})
+// })
